efccf6a625b6305ef93bc68be1827b88e975f677
AdamaDaniel <129950203+AdamaDaniel@users.noreply.github.com>
2024-04-16 14:25:10 +0200

    Add files via upload
    Ajout d'un compteur 'chances'  qui se decremente de (-1) a chaque fausse tentative

--- devinette.py
+++ devinette.py
@@ -4,12 +4,22 @@
 from numpy import random
 x = random.randint(0,1000)
 n=0
-while x!=n:
+chance=5
+print("Jeu de devinette")
+print("Vous avez cinq (5) chances pour tenter de deviner le nombre")
+while x!=n and chance!=0:
     #demander a l'utilisateur d'entrer un nombre
     n=int(input("Devinez un nombre:"))
     if n<x:
-        print('le nombre que vous avez saisie est inferieur')
+        print(f"le nombre que vous avez saisie est inferieur il vous reste {chance-1} tentatives")
     elif n==x:
-       print('Bravo vous avez reuissi')
+       print('Bravo vous avez reuissi, a seulement ')
     else:
-        print('le nombre est superieur.')
+        print(f"le nombre est superieur  il vous reste {chance-1} tentatives.")
+
+        break
+    # decrementer chance a chaque tentative echoué
+    chance -= 1
+# Afficher le nombre toutes les tentatives sont voué a l'echec
+if chance == 0:
+    print(f"Vous avez utilisé toutes vos chances. Le nombre correct était {x}.")
