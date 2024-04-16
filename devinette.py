be78b71ca02b4d569fb6b4432139d915d1c98616
AdamaDaniel <129950203+AdamaDaniel@users.noreply.github.com>
2024-04-16 13:40:56 +0200

    Update devinette.py
    commentaires

--- devinette.py
+++ devinette.py
@@ -1,13 +1,15 @@
 # Jeu de devinette
 #La commande randint(a,b) permet d'obtenir un entier aléatoire dans l'intervalle [a ; b]
+#importer random
 from numpy import random
 x = random.randint(0,1000)
 n=0
 while x!=n:
+    #demander a l'utilisateur d'entrer un nombre
     n=int(input("Devinez un nombre:"))
     if n<x:
-        print('le nombre que vous avez saisi est inferieur')
+        print('le nombre que vous avez saisie est inferieur')
     elif n==x:
-       print('Bravo')
+       print('Bravo vous avez reuissi')
     else:
-        print('le nombre est superieur')
+        print('le nombre est superieur.')
